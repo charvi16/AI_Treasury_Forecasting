83cff6013b654a450ef5cec0ff8bc3d6295f1c6c
Charvi Agarwal <charviagarwal2004@gmail.com>
2025-12-02 22:39:18 +0530

    final commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// export const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000"
+// });
+
+// export const getForecastData = () => API.get("/forecast");
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
 export const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: API_BASE
 });
 
 export const getForecastData = () => API.get("/forecast");
